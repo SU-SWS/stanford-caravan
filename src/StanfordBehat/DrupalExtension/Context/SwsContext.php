cf70f168e8e56c5a3997ceeddaa7bd4c5ae2d511
John Bickar <jbickar@stanford.edu>
2020-03-06 10:50:31 -0800

    D8CORE-1351: Add "Given I am logged in as User One" custom step definition

--- src/StanfordBehat/DrupalExtension/Context/SwsContext.php
+++ src/StanfordBehat/DrupalExtension/Context/SwsContext.php
@@ -248,5 +248,16 @@ class SwsContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   }
 
+  /**
+   * @Given I am logged in as User One
+   */
+  public function iAmLoggedInAsUserOne() {
+    // Check if logged in.
+    if ($this->loggedIn()) {
+      $this->logout();
+    }
+    $url = $this->getDriver()->drush('user-login 1 --browser=0');
+    $this->visitPath($url);
+  }
 
 }
