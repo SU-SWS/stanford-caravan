9010981eef5bd310278184361142dcd4bfa7e9d7
Mike Decker <pookmish@gmail.com>
2020-06-03 22:41:33 -0700

    composer create project for D8 only right now

--- src/Robo/Tasks/SuDrupalStack.php
+++ src/Robo/Tasks/SuDrupalStack.php
@@ -96,7 +96,7 @@ class SuDrupalStack extends BaseTask implements BuilderAwareInterface {
     // Create the project.
     // @link https://www.drupal.org/docs/develop/using-composer/using-composer-to-install-drupal-and-manage-dependencies
     $this->taskComposerCreateProject()
-      ->arg('drupal/recommended-project')
+      ->arg('drupal/recommended-project:^8')
       ->arg($this->path)
       ->option('no-interaction')
       ->option('no-install')
