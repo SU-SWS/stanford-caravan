2411ce7bdef7c8db11deaa17a3d934687e28e780
Mike Decker <pookmish@gmail.com>
2022-06-27 12:26:07 -0700

    -W

--- src/Robo/Tasks/SuCodeCeption.php
+++ src/Robo/Tasks/SuCodeCeption.php
@@ -219,7 +219,7 @@ class SuCodeCeption extends BaseTask implements BuilderAwareInterface {
    *
    */
   public function splitTests() {
-    $this->taskExec('composer global require codeception/codeception:^4.0')
+    $this->taskExec('composer global require codeception/codeception:^4.0 -W')
       ->run();
     $this->taskSplitTestFilesByGroups(self::NUMBER_OF_GROUPS)
       ->projectRoot($this->path)
