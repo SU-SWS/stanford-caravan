92a5086150c5869dc521c2e84d35c94741d29efd
Mike Decker <pookmish@gmail.com>
2022-06-28 10:37:11 -0700

    Xpath

--- src/Robo/Tasks/SuCodeCeption.php
+++ src/Robo/Tasks/SuCodeCeption.php
@@ -214,11 +214,14 @@ class SuCodeCeption extends BaseTask implements BuilderAwareInterface {
       $result_markup = file_get_contents("{$this->path}/artifacts/$suite/retry.html");
     }
     if ($gh_summary = getenv('GITHUB_STEP_SUMMARY')) {
+      libxml_use_internal_errors(TRUE);
+
       $markdown_converter = new HtmlConverter();
       $dom = new \DOMDocument();
       $dom->loadHTML($result_markup);
+      $xpath = new \DOMXPath($dom);
       $body = $xpath->query('//body');
-      file_put_contents($gh_summary, $markdown_converter->convert($body[0]->nodeValue));
+      file_put_contents($gh_summary, (string) $markdown_converter->convert($body[0]->nodeValue));
     }
     return $test;
   }
