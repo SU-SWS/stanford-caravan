b4e32e3ad08eaa8670880998d3584bbc6f7ad295
pookmish <pookmish@stanford.edu>
2025-06-20 05:12:10 +0700

    Change composer merge behavior (#26)

--- src/Robo/Tasks/SuDrupalStack.php
+++ src/Robo/Tasks/SuDrupalStack.php
@@ -236,7 +236,7 @@ class SuDrupalStack extends BaseTask implements BuilderAwareInterface {
     foreach ($merge_keys as $merge_key) {
       if (isset($composer_to_add[$merge_key])) {
         if (isset($composer[$merge_key])) {
-          $composer[$merge_key] = self::arrayMergeRecursive($composer_to_add[$merge_key], $composer[$merge_key]);
+          $composer[$merge_key] = self::arrayMergeRecursive($composer[$merge_key], $composer_to_add[$merge_key]);
           continue;
         }
 
